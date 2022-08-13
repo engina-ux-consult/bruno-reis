15b3af4a64f2db2c4080877e720c9d84fbce20f3
Reidner Filho <reidnerfilho@nasajon.con.br>
2022-08-13 17:04:21 -0300

    teste na tela principal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,26 +7,12 @@ import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from '../utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-  const theme = createTheme({});
-
+export default function MyApp() {
+  
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+   <div>
+    <h1>TESTE</h1>
+   </div>
   );
 }
