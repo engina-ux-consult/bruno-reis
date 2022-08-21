2477d064dec77a8148f695276910d5733c233d0d
Reidner Filho <reidnerfilho@nasajon.con.br>
2022-08-20 23:57:11 -0300

    Tela de Criação de Conta

--- pages/criarconta.tsx
+++ pages/criarconta.tsx
@@ -2,26 +2,75 @@ import Head from 'next/head'
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
 import Link from 'next/link';
 import React, {useState} from "react";
-import Planos from './planos';
-import Pagamento from './checkout';
-import Cadastro from './cadastro';
-const steps = [
-    'Conta',
-    'Planos',
-    'Pagamento',
-  ];
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { ClassNames } from '@emotion/react';
 
+interface State {
+    amount: string;
+    password: string;
+    weight: string;
+    weightRange: string;
+    showPassword: boolean;
+  }
+
+
+  
 function IndexPage() {
-const [page, setPage] = useState(0);
-const [step, setStep] = useState(0);
+    const [formValues, setFormValues] = useState({});
 
-const FormTitles = [<Cadastro/>, <Planos/>, <Pagamento/>];
+    const handleInputChange = (e:any) => {
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value})
+    };
 
+    const [values, setValues] = React.useState<State>({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+      });
+    
+      const handleChange =
+        (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+          setValues({ ...values, [prop]: event.target.value });
+        };
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+      };
+
+    console.log('*** formValues', formValues);
+
+    const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
+
+  const handleOnChange = (e: any) => {
+    setEmail(e.target.value);
+    if (e.target.value.match(regEx)) {
+      setMessage("");
+      return
+    } else {
+      setMessage("Email não é valido");
+    }
+  };
   return (
 
       <div>
@@ -34,48 +83,65 @@ const FormTitles = [<Cadastro/>, <Planos/>, <Pagamento/>];
         <Grid container style={{ minHeight: '100vh'}}>
             
             <Grid item xs={12} sm={6} style={{ position: 'relative', alignItems: 'center', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center',}}>
-                <h1 style={{ maxWidth: '650px', fontSize: '2.5rem', color: 'white', position: 'absolute', padding: '64px', }} >Lorem ipsum class sodales vivamus aptent, quis sagittis euismod dictumst nulla.</h1>
-                <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg" style={{ width: '100%', height: '100vh', objectFit: 'cover'}} alt="brand" />
+                <img src="../imgs/bd-broe.jpg" style={{ width: '100%', height: '100vh', objectFit: 'cover'}} alt="brand" />
             </Grid>
 
             <Grid container item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" style={{ justifyContent: 'center', padding: 16,}}>
 
                 <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 400, }}>
 
-                    <Grid container justify="center" style={{ justifyContent: 'center'}}>
-                            <img src="../imgs/logo-brand.svg" width={200} alt="logo" />
-                     </Grid>
-
-                    <div style={{height:40}} />
-
-                     <div style={{ justifyContent: 'center'}}>
-                        <Stepper activeStep={step} alternativeLabel>
-                            {steps.map((label) => (
-                            <Step key={label}>
-                                <StepLabel>{label}</StepLabel>
-                           </Step>
-                           ))}
-                      </Stepper>
-                     </div>
-                        
-                    <div style={{height:40}} />
-
-                    <div>{FormTitles[page]}</div>
-
-                    <div style={{height:40}} />
-                        <Button variant="contained" type="submit"
-                        onClick={() => {
-                            setPage((currPage) => currPage + 1)
-                            setStep((activeStep) => activeStep + 1)
-                        }}>Continuar</Button>
-
+                <Grid container justify="center" style={{ justifyContent: 'center'}}>
+                                <img src="../imgs/logo-brandd.svg" width={200} alt="logo" />
+                        </Grid>
+                    
                         <div style={{height:40}} />
+                        <form>
+                <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 400, }}>
+                    
+                    <div style={{height:40}} />
+                    <TextField sx={{['& fieldset']:{borderRadius:4}}} type="text" id="nome" label="Nome" name="name" variant="outlined" borderRadius='16' placeholder="nome" onChange={handleInputChange} value={formValues.name || ''} />
+                    <div style={{height:40}} />
+                    <TextField sx={{['& fieldset']:{borderRadius:4}}} type="email" id="email" label="E-mail" variant="outlined" name="email" 
+                    onChange={handleOnChange} value={email || ''} error={message} helperText={message}/>
+                    <div style={{height:40}} />
+                    <FormControl sx={{ m: 0, width: '45ch',['& fieldset']:{borderRadius:4} }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Senha"
+          />
+        </FormControl>   
+                </div>
+            </form>
+                        <div style={{height:40}} />
+                            <Button variant="contained" type="submit"
+                            sx={{
+                                backgroundColor: '#382B57',
+                                borderRadius: 8
+                              }}>Continuar</Button>
 
-                        <Link href="/login" passHref>
-                        <Button>Já possui uma conta?</Button>
-                        </Link>
-                    </div>
-            </Grid>
+                            <div style={{height:40}} />
+
+                            
+                            <Button>Já possui uma conta?<Link href="/login" passHref>Acesse aqui</Link> </Button>
+                          
+                        </div>
+                </Grid>
 
         </Grid>
       </div>
