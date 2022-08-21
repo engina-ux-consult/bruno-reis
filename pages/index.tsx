bb6fe9506c0b69f6d01647a39000cc1c2c8b2a45
Reidner Filho <reidnerfilho@nasajon.con.br>
2022-08-21 01:43:35 -0300

    Tela Principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
 import type { NextPage } from 'next'
-import Button from '@mui/material/Button';
-
+import IndexPage from '../pages/criarconta'
 
 const Home: NextPage = () => {
   return (
-    <body>
-
-      <div>
-        <h1>Lorem ipsum class sodales vivamus aptent, quis sagittis euismod dictumst nulla.</h1>
-      </div>
-
-      <div>
-      <Button variant="contained">Contained</Button>
-      </div>
-
-    </body>
-
-  )
+    <div>
+      <IndexPage></IndexPage>
+    </div>
+  );
 }
 
 export default Home
