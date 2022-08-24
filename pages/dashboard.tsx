86e645a2ed5b1ae8b71ef0d90f4a670d2aff9e24
Reidner Filho <reidnerfilho@nasajon.con.br>
2022-08-24 03:25:16 -0300

    Dashboard part 3

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,91 +4,26 @@ import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { styled, alpha } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import { Instagram, LinkedIn, YouTube } from '@mui/icons-material';
+import { ArrowForward, Home, Instagram, LinkedIn, Output, YouTube } from '@mui/icons-material';
 import { Button, Icon } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
-
-const drawerWidth = 240;
  function Dashboard() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
   };
 
   const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
@@ -96,85 +31,16 @@ const drawerWidth = 240;
   };
 
   const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+  
   return (
     <Box sx={{ display: 'flex'}} >
       <CssBaseline />
       {/*Toolbar Horizontal*/}
       <AppBar
         position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` ,background: 'transparent', boxShadow: 'none'}}
+        sx={{ width: `calc(100% - ${'110'}px)`, ml: `${'110'}px` ,background: 'transparent', boxShadow: 'none'}}
       >
         <Toolbar>
           <Typography
@@ -186,6 +52,14 @@ const drawerWidth = 240;
             Olá, Fulano
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' }, color: '#000000' }}
+          >
+           Siga as redes sociais
+          </Typography>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
               <Badge>
@@ -230,17 +104,32 @@ const drawerWidth = 240;
       {/*Toolbar Vertical*/}
       <Drawer
         sx={{
-          width: drawerWidth,
+          width: '110px',
           flexShrink: 0,
           '& .MuiDrawer-paper': {
-            width: drawerWidth,
+            width: '88px',
+            
             boxSizing: 'border-box',
+            background: '#382B57'
           },
         }}
         variant="permanent"
         anchor="left"
       >
         <Button>B</Button>
+        <IconButton
+              size="small"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+            <Button variant="contained" style={{ color: "#FFFFFF", background: "#584389",borderRadius: 3 }} startIcon={<Home />}/>
+        </IconButton>
+      
+          <Button style={{ color: "#FFFFFF", position: 'relative'}} startIcon={<Output />}/>
+       
       </Drawer>
       <Box
         component="main"
@@ -249,31 +138,38 @@ const drawerWidth = 240;
         <Toolbar />
         <Grid item xs={12} sm={6} style={{borderRadius:'0.5rem', color:'white', backgroundColor: '#382B57',width: '100%', height: '18.50rem', objectFit: 'cover'}}>
           <Grid style={{position: "absolute",margin: '56px 32px'}}>
-            <Typography  style={{width: '365px',height: '120px',fontFamily: 'Inter',fontStyle: 'normal',fontWeight: '600',fontSize: '32px',lineHeight: '40px'}}>
+            <Typography  style={{width: '365px',height: '120px',fontStyle: 'normal',fontWeight: '600',fontSize: '32px',lineHeight: '40px'}}>
               Plataforma exclusiva para compartilhamento de conteúdos
             </Typography> 
-            <Typography style={{width: '365px',height: '48px',fontFamily: 'Open Sans',fontStyle: 'normal',fontWeight: '400',fontSize: '16px',lineHeight: '24px'}} paragraph>
+            <Typography style={{width: '365px',height: '48px',fontStyle: 'normal',fontWeight: '400',fontSize: '16px',lineHeight: '24px'}} paragraph>
               Aqui você encontra os melhores conteúdos e conhecimentos do mundo da odontologia.
             </Typography>  
               <img src="../imgs/logo-trans.png"  style={{position: "fixed",width: "319px",height: "288px",left: "969px",top: "96px" }} alt="brand" />
           </Grid>
         </Grid>
-        <Typography variant='subtitle1' marginTop={'32px'}>
-        Conteúdos
+        <Typography variant='subtitle1' style={{fontWeight: '600', fontSize: '24px'}} marginTop={'32px'} marginBottom={'32px'}>
+          Conteúdos
         </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
+        <Card sx={{ maxWidth: 397 }}>
+      <CardMedia
+        component="img"
+        alt="Guia Resinas"
+        height="264"
+        width="397"
+        image="../imgs/guia-resinas.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Guia completo de resinas compostas
         </Typography>
+        <Typography variant="body2" color="text.secondary">
+        Este conteúdo foi desenvolvido para auxiliar cirurgiões-dentistas no momento de escolha de uma resina composta.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" sx={{backgroundColor: '#382B57',borderRadius: 3}} endIcon={<ArrowForward />}>Ler conteúdo</Button>
+      </CardActions>
+    </Card>
       </Box>
     </Box>
   );
