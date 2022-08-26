a3e3d463e4b1e73de35df04b85a6a9e99788cdfb
Reidner Filho <reidnerfilho@nasajon.con.br>
2022-08-26 01:15:36 -0300

    Dashboard part 5

--- pages/guia-resinas.tsx
+++ pages/guia-resinas.tsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import { ArrowForward, Home, Instagram, LinkedIn, Output, YouTube } from '@mui/icons-material';
+import { ArrowForward, Home, Instagram, LinkedIn, Output, YouTube, ArrowBack, Bolt } from '@mui/icons-material';
 import { Button, Icon } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
@@ -16,8 +16,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Link from 'next/link';
-
- function Dashboard() {
+ function guiaResinas() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null);
@@ -60,13 +59,16 @@ import Link from 'next/link';
         sx={{ width: `calc(100% - ${'110'}px)`, ml: `${'110'}px` ,background: '#FFFFFF', boxShadow: 'none'}}
       >
         <Toolbar>
+        <Link href="/dashboard" passHref>
+            <Button style={{ color: "#000000", float: "left"}} startIcon={<ArrowBack />}/>
+        </Link>
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' }, color: '#000000' }}
           >
-            Olá, Fulano
+            Guia completo de resinas compostas
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Typography
@@ -75,33 +77,13 @@ import Link from 'next/link';
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' }, color: '#000000' }}
           >
-           Siga as redes sociais
+           Conteúdo exclusivo
           </Typography>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
               <Badge>
-                <Instagram style={{ color: "#382B57" }}/>
+                <Bolt style={{ color: "#382B57" }}/>
               </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge >
-                <YouTube style={{ color: "#382B57" }}/>
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <LinkedIn style={{ color: "#382B57" }}/>
             </IconButton>
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
@@ -148,54 +130,18 @@ import Link from 'next/link';
         </Link> 
         <Link href="/" passHref>
           <Button style={{ color: "#FFFFFF", position: 'absolute', bottom: '0'}} startIcon={<Output />}/>
-        </Link>
-       
+        </Link>  
       </Drawer>
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
       >
         <Toolbar />
-        <Grid item xs={12} sm={6} style={{borderRadius:'0.5rem', color:'white', backgroundColor: '#382B57',width: '100%', height: '18.50rem', objectFit: 'cover'}}>
-          <Grid style={{position: "absolute",margin: '56px 32px'}}>
-            <Typography  style={{width: '365px',height: '120px',fontStyle: 'normal',fontWeight: '600',fontSize: '32px',lineHeight: '40px'}}>
-              Plataforma exclusiva para compartilhamento de conteúdos
-            </Typography> 
-            <Typography style={{width: '365px',height: '48px',fontStyle: 'normal',fontWeight: '400',fontSize: '16px',lineHeight: '30px'}} paragraph>
-              Aqui você encontra os melhores conteúdos e conhecimentos do mundo da odontologia.
-            </Typography>  
-            </Grid>
-              <img src="../imgs/logo-trans.png"  style={{float: "right" ,width: "319px",height: "288px",left: "969px",top: "96px", margin: '6px 120px'}} alt="brand" /> 
-        </Grid>
-        <Typography variant='subtitle1' style={{fontWeight: '600', fontSize: '24px'}} marginTop={'32px'} marginBottom={'32px'}>
-          Conteúdos
-        </Typography>
-        <Card sx={{ maxWidth: 397 }}>
-      <CardMedia
-        component="img"
-        alt="Guia Resinas"
-        height="264"
-        width="397"
-        image="../imgs/guia-resinas.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Guia completo de resinas compostas
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Este conteúdo foi desenvolvido para auxiliar cirurgiões-dentistas no momento de escolha de uma resina composta.
-        </Typography>
-      </CardContent>
-      <CardActions>
-      <Link href="/guia-resinas" passHref>
-        <Button variant="contained" sx={{backgroundColor: '#382B57',borderRadius: 3}} endIcon={<ArrowForward />}>Ler conteúdo</Button>
-      </Link>
-      </CardActions>
-    </Card>
+        <img src="../imgs/bg-resinas.jpg"  style={{width: "100%",height: "300px"}} alt="brand" /> 
       </Box>
     </Box>
         )}
     </>
   );
 }
-export default Dashboard
+export default guiaResinas
